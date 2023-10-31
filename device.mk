bbf34a923eb97453a2e61ec5f4914028bb6d3f0c
ugly-kid-af <dwarmachine24@gmail.com>
2023-10-31 20:38:30 +0530

    Unwanted Apps Removal Script

--- device.mk
+++ device.mk
@@ -273,6 +273,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6877 \
